52ca8c45cb512923ff36fbd2dc73b85870192b2c
Ka <carla_cdssantos@hotmail.com>
2019-02-28 11:47:44 -0300

    go

--- catalogo_filmes/settings.py
+++ catalogo_filmes/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
